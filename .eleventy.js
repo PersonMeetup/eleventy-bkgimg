107a49330973bbe23ffd88b493c0c2b5ffd7d420
Leslie Swan <contact@personmeetup.ca>
2026-04-11 13:39:14 -0600

    Feature complete

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 import eleventyBkgimg from "./index.js";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 /** @param {import("@11ty/eleventy/UserConfig").default} eleventyConfig */
 export default function (eleventyConfig) {
 	eleventyConfig.addPlugin(eleventyBkgimg, {
 		extensions: "html,njk,liquid,css,webc",
 	});
-	eleventyConfig.setInputDirectory('test');
+	eleventyConfig.setInputDirectory("test");
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 }
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import EleventyImage from "@11ty/eleventy-img";
-import DEFAULTS from "@11ty/eleventy-img/src/global-options.js";
+import { Util } from "@11ty/eleventy-img";
 import path from "node:path";
-import { copyFileSync } from "node:fs";
 
 /**
  * Searches for `url()` values within templates and stylesheets,
@@ -10,19 +9,28 @@ import { copyFileSync } from "node:fs";
  * NOTE: When I get around to properly developing this as a standalone
  * plugin, keep note of the work done in the transform plugin:
  * https://github.com/11ty/eleventy-img/blob/main/src/transform-plugin.js#L64
- * @param {import("@11ty/eleventy/UserConfig").default} eleventyConfig
- * @param {import("@11ty/eleventy-img/").ImageOptions} options Eleventy Image config
+ * @param {import("@11ty/eleventy/UserConfig").default} eleventyConfig User-land configuration instance
+ * @param {import("@11ty/eleventy-img/").ImageOptions} options Eleventy Image options
  */
 export default function (eleventyConfig, options = {}) {
 	const INPUT_DIR = eleventyConfig.directories.input;
 	const OUTPUT_DIR = eleventyConfig.directories.output;
 
 	/** @type {import("@11ty/eleventy-img/").ImageOptions}  */
-	let pluginOptions = Object.assign({
-		// If urlPath not set, assume default setting
-		outputDir: path.join(OUTPUT_DIR, options.urlPath || "/img/"),
-		extensions: "html,njk,liquid,css",
-	}, options);
+	let pluginOptions = Object.assign(
+		{
+			// If urlPath not set, assume default setting
+			outputDir: path.join(OUTPUT_DIR, options.urlPath || "/img/"),
+			extensions: "html,njk,liquid,css",
+		},
+		options,
+	);
+
+	async function parseBackgrounds(data, content) {}
+
+	function replaceContext(content, context, orgUrl, newUrl) {
+		return content.replace(context, context.replace(orgUrl, newUrl));
+	}
 
 	// TODO: Make sure this doesn't conflict with other plugins, such as the PostCSS one!
 	eleventyConfig.addTemplateFormats("css");
@@ -35,7 +43,6 @@ export default function (eleventyConfig, options = {}) {
 		},
 	});
 
-	// TODO: During watch, images without formatting options default to lowest resolution?
 	eleventyConfig.addPreprocessor(
 		"bkgimg",
 		pluginOptions.extensions,
@@ -43,32 +50,39 @@ export default function (eleventyConfig, options = {}) {
 			let dataErrors = [];
 			if (typeof content == "string") {
 				// Grab the `url()` snippet, check for image
-				let images = content
-					.match(/url\(.*\)/g)
-					?.filter((url) => !url.includes(".css"));
+				let images = [];
+				const backgrounds = content.matchAll(
+					/(?:.|\s){2,9}(?<!\/\*)background(?:.?|-image):.*url\(.*\)(?:.|\s){1,9}/g,
+				);
+				for (const bkg of backgrounds) {
+					const url = bkg[0].match(/url\((?:'?|"?)(.*)(?:'?|"?)\)/).at(1);
+					if (url !== "" || !url.includes(".css"))
+						images.push({
+							bkgCtx: bkg[0],
+							bkgUrl: url,
+						});
+				}
 				if (!images) return content;
+				// set defaults as seperate object
 
 				image: for (let img of images) {
-					// Remove function syntax from address
-					if (img.match(/url\(("|').*\)/g))
-						img = img.slice(5, -2);
-					else
-						img = img.slice(4, -1);
+					const { inputPath, filePathStem } = data.page;
+					const { bkgUrl, bkgCtx } = img;
+					const address = Util.normalizeImageSource(
+						{ input: INPUT_DIR, inputPath },
+						bkgUrl,
+					);
+					/** @type {import("@11ty/eleventy-img/").ImageOptions}  */
+					let imageOptions = structuredClone(pluginOptions);
 
 					// Check if the address is local or external
 					let local = false;
-					let address = img;
-					if (!URL.canParse(address)) {
-						local = true
-						if (!path.isAbsolute(img)) {
-							address = path.join(data.page.fileSlug, img);
-						}
-						// Add a fake address to the start for the URL object
-						address = path.join('a:/', address);
-					}
-					const url = new URL(address);
+					if (!URL.canParse(address)) local = true;
 
-					for (let [key, value] of url.searchParams) {
+					const params = new URL(path.join(local ? "a:/" : "", address)).search;
+					const imgSrc = address.replace(decodeURIComponent(params), "");
+
+					for (let [key, value] of new URLSearchParams(params)) {
 						// TODO: Aim for parity with https://www.11ty.dev/docs/plugins/image/#attribute-overrides
 						switch (key) {
 							case "widths":
@@ -77,91 +91,114 @@ export default function (eleventyConfig, options = {}) {
 									value = JSON.parse(value);
 									if (!Array.isArray(value)) value = [value];
 									if (value.length > 1)
-										dataErrors.push(`'widths': Warning: Multiple values given, will use smallest option (${img})`)
-									value.forEach(check => {
-										if (typeof (check) == "string" && !(check == "auto"))
-											throw new Error('Invalid string value (Did you mean to set "auto"?)');
+										dataErrors.push(
+											`'widths': Warning: Multiple values given, will use smallest option (${bkgUrl})`,
+										);
+									value.forEach((check) => {
+										if (
+											typeof check == "string" &&
+											isNaN(parseInt(check)) &&
+											check != "auto"
+										)
+											throw new Error(
+												'Invalid string value (Did you mean to set "auto"?)',
+											);
 									});
-									pluginOptions.widths = value;
+									imageOptions.widths = value;
 								} catch (error) {
-									pluginOptions.widths = ["auto"];
-									dataErrors.push(`'widths': ${error} (${img})`)
+									imageOptions.widths = ["auto"];
+									dataErrors.push(`'widths': ${error} (${bkgUrl})`);
 								}
 								break;
 
 							case "formats":
 							case "format":
-
-								// TODO: is this needed?
-								/** @type {import("@11ty/eleventy-img/").ImageFormat}  */
-								const FORMATS = ['webp', 'jpeg', 'png', 'svg', 'avif', 'gif', 'auto'];
 								try {
 									value = JSON.parse(value);
 									if (!Array.isArray(value)) value = [value];
 									if (value.length > 1)
-										dataErrors.push(`'widths': Warning: Multiple values given, will use first option (${img})`)
-									value.forEach(check => {
-										if (!typeof (check) == "string" || !(FORMATS.includes(check)))
-											throw new Error('Invalid string value (Did you mean to set "auto"?)');
-									});
-									pluginOptions.formats = value;
-									pluginOptions.formats = ['']
+										dataErrors.push(
+											`'formats': Warning: Multiple values given, will use first option (${bkgUrl})`,
+										);
+									// We can't do much preemptive filtering, but we can look for strings
+									if (!typeof value[0] == "string")
+										throw new Error("Invalid value type, expected string");
+									imageOptions.formats = value;
 								} catch (error) {
-									pluginOptions.formats = ["auto"];
-									dataErrors.push(`'formats': ${error} (${img})`)
+									imageOptions.formats = ["auto"];
+									dataErrors.push(`'formats': ${error} (${bkgUrl})`);
 								}
 								break;
 
 							case "ignore":
-								if (local) {
-									let src = url.pathname;
-									copyFileSync(path.join(INPUT_DIR, src), path.join(OUTPUT_DIR, src));
-									content = content.replace(img, src);
-								}
+								content = replaceContext(
+									content,
+									bkgCtx,
+									bkgUrl,
+									bkgUrl.replace(decodeURIComponent(params), ""),
+								);
 								continue image;
-
-							case "optional":
-								if (value == "keep") { }
-								if (value == "placeholder") { }
-								break;
 
 							// May not be the best idea???
 							case "output":
-
+								try {
+									// It's okay if this fails; we just needed to clean quotes
+									value = JSON.parse(value);
+								} catch {}
+								if (path.isAbsolute(value)) {
+									imageOptions.outputDir = path.join(OUTPUT_DIR, value);
+									imageOptions.urlPath = value;
+								} else {
+									imageOptions.outputDir = path.join(
+										OUTPUT_DIR,
+										filePathStem,
+										value,
+									);
+									imageOptions.urlPath = path.join(filePathStem, value);
+								}
 								break;
 
 							default:
-								dataErrors.push(`Invalid argument: ${key}(${img})`)
-						}
-					};
-
-					let src = '';
-					if (local)
-						src = path.join(INPUT_DIR, url.pathname);
-					else
-						src = url.href;
-
-					// Simplify options
-					if (pluginOptions.widths?.length > 1) {
-						const index = pluginOptions.widths.findIndex((i) => i == "auto");
-						if (typeof (index) == "number") {
-							pluginOptions.widths = [Math.min(
-								...pluginOptions.widths.toSpliced(index, 1, Infinity)
-							)];
+								dataErrors.push(`Invalid argument: ${key} (${bkgUrl})`);
 						}
 					}
-					pluginOptions.formats = [pluginOptions.formats?.at(0) || 'webp'];
 
-					let imgPath = await EleventyImage(src, pluginOptions);
-					content = content.replace(img, imgPath[pluginOptions.formats[0]][0].url);
+					// Simplify/set options
+					if (imageOptions.widths?.length > 1) {
+						const index = imageOptions.widths.findIndex((i) => i == "auto");
+						if (typeof index == "number") {
+							imageOptions.widths = [
+								Math.min(...imageOptions.widths.toSpliced(index, 1, Infinity)),
+							];
+						}
+					}
+					imageOptions.formats = [imageOptions.formats?.at(0) || "webp"];
+
+					try {
+						let imgPath = await EleventyImage(imgSrc, imageOptions);
+						content = replaceContext(
+							content,
+							bkgCtx,
+							bkgUrl,
+							imgPath[Object.keys(imgPath)[0]][0].url,
+						);
+					} catch (err) {
+						dataErrors.push(`${err} (${bkgUrl})`);
+						content = replaceContext(
+							content,
+							bkgCtx,
+							bkgUrl,
+							bkgUrl.replace(decodeURIComponent(params), ""),
+						);
+					}
 				}
 			}
 
 			if (dataErrors.length > 0) {
 				let msg = `Problems while preprocessing url() functions in ${data.page.inputPath} `;
-				let i = 1
-				dataErrors.forEach(error => {
-					msg += `\n${i++}. ${error} `
+				let i = 1;
+				dataErrors.forEach((error) => {
+					msg += `\n${i++}. ${error} `;
 				});
 				eleventyConfig?.logger?.logWithOptions({
 					message: msg,
